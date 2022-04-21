59f5cf9dd2fd871bb4c23265a813408ece4b5470
이보람 <boram@ibolamucBookAir.skbroadband>
2022-04-21 19:24:57 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import { useState, useEffect } from "react";
-
-function Hello() {
-  // function byeFn() {
-  //   console.log("bye :(");
-  // };
-
-  // function hiFn() {
-  //   console.log("created :)");
-  //   return byeFn; // component가 언제 파괴될지 알 수 있다.
-  // };
-
-  // useEffect(() => {
-  //   console.log("hi :)");
-  //   return () => console.log("bye :(");
-  // }, []);
-
-  useEffect(function() {
-    console.log("hi :)");
-    return function() {
-      console.log("bye :(")
-    }
-  }, []);
-  // useEffect(hiFn, []);
-
-  // useEffect(() => {
-  //   console.log("created :)");
-  //   return () => console.log("destroyed :("); // component가 destroy될 때 실행
-  // }, []);
-
-  return <h1>Hello</h1>;
-};
+import style from "./App.module.css";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [myMoney, setMyMoney] = useState(0);
+    const [coinPrice, setCoinPrice] = useState(0);
 
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+    useEffect(() => { // 한 번만 실행할 함수
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then(res => res.json())
+        .then((res) => {
+            setCoins(res); // coins array에 불러온 값 담기
+            setLoading(false); // false로 변환
+        })
+    }, []);
+
+    const writeMoney = (e) => setMyMoney(e.target.value);
+    const selectCoin = (e) => setCoinPrice(e.target.value);
+
+    return ( <div>
+        <h1 className={style.title}>THE COINS! 😄💰</h1>
+        {loading ? 
+        <strong>Loading...</strong> :
+        <select onChange={selectCoin}>
+            <option key="-1">Select coin</option>
+            {coins.map((coin, index) => 
+            <option key={index} value={coin.quotes.USD.price.toFixed(2)}>
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price.toFixed(2)}
+            </option>)}
+        </select>
+        }
+        <div>
+            <input type="number"
+            value={myMoney}
+            onChange={writeMoney}></input>
+        </div>
+        <div>
+            <h3>Coins you can buy : {coinPrice ? (myMoney / coinPrice).toFixed(2) : null}</h3>
+        </div>
+        
+    </div>)
 };
 
 export default App;
