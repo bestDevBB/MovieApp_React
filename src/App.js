6f39b0ac7599e7b91d5985a6cc4e9afc81da6d57
이보람 <boram@192.168.nate.com>
2022-05-04 15:25:01 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import { useState, useEffect } from "react";
-import style from "./App.module.css";
+import { useState } from "react";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [myMoney, setMyMoney] = useState(0);
-    const [coinPrice, setCoinPrice] = useState(0);
-
-    useEffect(() => { // 한 번만 실행할 함수
-        fetch("https://api.coinpaprika.com/v1/tickers")
-        .then(res => res.json())
-        .then((res) => {
-            setCoins(res); // coins array에 불러온 값 담기
-            setLoading(false); // false로 변환
-        })
-    }, []);
-
-    const writeMoney = (e) => setMyMoney(e.target.value);
-    const selectCoin = (e) => setCoinPrice(e.target.value);
-
-    return ( <div>
-        <h1 className={style.title}>THE COINS! 😄💰</h1>
-        {loading ? 
-        <strong>Loading...</strong> :
-        <select onChange={selectCoin}>
-            <option key="-1">Select coin</option>
-            {coins.map((coin, index) => 
-            <option key={index} value={coin.quotes.USD.price.toFixed(2)}>
-                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price.toFixed(2)}
-            </option>)}
-        </select>
-        }
-        <div>
-            <input type="number"
-            value={myMoney}
-            onChange={writeMoney}></input>
-        </div>
-        <div>
-            <h3>Coins you can buy : {coinPrice ? (myMoney / coinPrice).toFixed(2) : null}</h3>
-        </div>
-        
-    </div>)
+    const [loading, setLoading] = useState(true); // default
+    // 첫 번째 item이 data, 두 번째는 dat를 수정할 수 있는 함수
+    return <div></div>
 };
 
 export default App;
