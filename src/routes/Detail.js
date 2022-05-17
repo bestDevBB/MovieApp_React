c888ce443030c853c5dcc5cb96b33ac05a0e2a8c
Boram Lee <cnn93@naver.com>
2022-05-18 00:16:49 +0900

    Movie App

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,13 +4,14 @@ import { useParams } from "react-router-dom";
 function Detail() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    console.log("type:", typeof movies);
 
     const { id } = useParams();
-    const getMovies = async () => {
+    const getMovies = async() => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`))
             .json();
-            // console.log(json);
+            setMovies(json.data.movies);
             setLoading(false);
         };
 
@@ -19,7 +20,9 @@ function Detail() {
     }, []);
 
     return <div>
-        <h1>Detail</h1>
+        {loading
+        ? <h1>Loading...</h1>
+        : "ddd"}
     </div>
 };
 
