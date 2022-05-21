0112f888ebcf242132450d4ba937df775aa9631b
Boram Lee <cnn93@naver.com>
2022-05-21 15:07:01 +0900

    Movie App

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 function Detail() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
-    console.log("type:", typeof movies);
 
     const { id } = useParams();
     const getMovies = async() => {
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ function Home() {
     // 첫 번째 item이 data, 두 번째는 data를 수정할 수 있는 함수
     const [loading, setLoading] = useState(true); // default
     const [movies, setMovies] = useState([]);
-    console.log(typeof movies);
+    // console.log(typeof movies);
 
     // useEffect를 이용해서 getMoives를 호출. then을 사용하지 않기 위해!
     // fetch가 진행된 후에 movies는 json.data.movies가 되는 것
